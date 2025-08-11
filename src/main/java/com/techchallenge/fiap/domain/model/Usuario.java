aaeafac21b01db1e33a8f816e598980f1d585553
Felipe Veiga <felipe.veiga.trabalho@gmail.com>
2025-08-11 11:22:19 -0300

    Entidade Usuario criada

---
+++ src/main/java/com/techchallenge/fiap/domain/model/Usuario.java
@@ -0,0 +1,108 @@
+package com.techchallenge.fiap.domain.model;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tbUsuario")
+public class Usuario {
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idUsuario;
+	
+	@Column(nullable = false)
+    private String nome;
+	
+	@Column(nullable = false, unique = true)
+	private String login;
+
+	@Column(nullable = false, unique = true)
+    private String email;
+    
+	@Column(nullable = false)
+    private String senha;
+    private LocalDateTime dataUltimaAlteracao;
+    
+    @OneToOne
+    @JoinColumn(name = "idEndereco", referencedColumnName = "idEndereco", nullable = false)
+    private Endereco endereco;
+    
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "idPerfil", nullable = false)
+    private Perfil perfil;
+    
+	public Usuario() {
+		super();
+	}
+	
+	public Long getIdUsuario() {
+		return idUsuario;
+	}
+	public void setIdUsuario(Long idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public LocalDateTime getDataUltimaAlteracao() {
+		return dataUltimaAlteracao;
+	}
+	public void setDataUltimaAlteracao(LocalDateTime dataUltimaAlteracao) {
+		this.dataUltimaAlteracao = dataUltimaAlteracao;
+	}
+	public Endereco getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+	public Perfil getPerfil() {
+		return perfil;
+	}
+	public void setPerfil(Perfil perfil) {
+		this.perfil = perfil;
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(idUsuario);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		return Objects.equals(idUsuario, other.idUsuario);
+	}
+}
